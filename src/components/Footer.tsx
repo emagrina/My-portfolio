52d564df2276f8c45986c12111663485b3374d3f
emagrina <ernest8081@gmail.com>
2023-04-23 04:48:45 +0200

    add: 📦 Implemented robots.txt and fixed the Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer
             className="footer footer-center p-4 bg-base-300 text-base-content"
-            style={{position: 'absolute', bottom: 0, width: '100%', height: '40px'}}
+            style={{position: 'absolute', bottom: 0, width: '100%'}}
         >
             <div>
                 <p>The website is under development ⚙️</p>
